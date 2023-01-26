4b47b8db6a12bf5fb4d1a984ec7a0071052e5875
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2023-01-26 16:41:25 +0300

    Added Ternary Search

---
+++ Ternary Search/code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int ternary_seach(int arr[], int length, int item);
+
+int main() {
+    int arr[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int index = ternary_seach(arr, length, 8);
+    cout << index;
+    return 0;
+}
+
+int ternary_seach(int arr[], int length, int item) {
+    int left = 0;
+    int right = length - 1;
+
+    while (left <= right) {
+        int h = (right - left) / 3;
+        int m1 = left + h;
+        int m2 = right - h;
+        if (arr[m1] == item) return m1;
+        if (arr[m2] == item) return m2;
+        if (arr[m1] < item && arr[m2] > item) {
+            left = m1 + 1;
+            right = m2 - 1;
+        }
+        else if (arr[m1] > item) right = m1 - 1;
+        else left = m2 + 1;
+    }
+
+    return -1;
+}
---
+++ Ternary Search/code.py
@@ -0,0 +1,19 @@
+def ternary_search(arr, item):
+	left = 0;
+	right = len(arr) - 1
+	while left <= right:
+		h = (right - left) // 3
+		m1 = left + h
+		m2 = right - h
+		if arr[m1] == item: return m1
+		if arr[m2] == item: return m2
+		if arr[m1] < item and arr[m2] > item:
+			left = m1 + 1
+			right = m2 - 1
+		elif arr[m1] > item: right = m1 - 1
+		else: left = m2 + 1
+	return -1
+
+arr = [-2, 0, 5, 6, 9, 10, 12, 15, 20]
+index = ternary_search(arr, 6)
+print(index)
